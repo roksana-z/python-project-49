c8c49ab0c297a271d4c13dabb55290336ff2c8db
roksana_zadoian <elleygj@gmail.com>
2023-06-06 23:13:03 +0200

    step 7

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.
 
 from brain_games.game import play_game
-from brain_games.games.brain_calc import get_correct_answer, get_question, show_game_description, show_question
+from brain_games.games.brain_gcd import get_correct_answer, get_question, show_game_description, show_question
 
 
 def main():
